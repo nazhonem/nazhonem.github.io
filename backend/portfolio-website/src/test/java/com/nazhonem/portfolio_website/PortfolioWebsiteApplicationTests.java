22088b2b5f620007bc48cc0a15f65ae76c8df870
nazhonem <namo2624@colorado.edu>
2024-10-23 16:15:17 -0600

    Setup
    This is mostly based on ChatGPT's recommendations. Still need to fix tailwind rule issue.

---
+++ backend/portfolio-website/src/test/java/com/nazhonem/portfolio_website/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazhonem.portfolio_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
