d143807ea8d628c664960fab913d9e60821998a0
nazhonem <namo2624@colorado.edu>
2025-08-30 12:41:19 -0600

    cleanup files

--- static-site/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import DevHome from './home';
-
-test('renders learn react link', () => {
-  render(<DevHome />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
